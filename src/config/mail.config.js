8f668db5e6bf3e8280fbe6de0ec2b3f72e9fbc62
Nghi Nguyen <ndnghi.2011@gmail.com>
2023-03-23 23:51:06 +0700

    html mail

--- src/config/mail.config.js
+++ src/config/mail.config.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 
 const config = {
   mailer: process.env.MAIL_MAILER,
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,7 +21,7 @@ const postLogin = async (req, res, next) => {
     }
 
     const account = await accountService.getAccountByUsername(username)
-        
+
     if (!account || !account.status) {
       throw customError(2, 'Tài khoản không tồn tại hoặc bị vô hiệu hóa')
     }
@@ -92,16 +92,37 @@ const postForgotPassword = async (req, res, next) => {
   const { username } = req.body
 
   try {
-    if(!username) throw customError(2, `Vui lòng nhập đầy đủ thông tin`)
+    if (!username) throw customError(2, `Vui lòng nhập đầy đủ thông tin`)
 
     const user = await userService.getUserId(username)
 
-    if(!user) throw customError(2, `Tài khoản không tồn tại`)
+    if (!user) throw customError(2, `Tài khoản không tồn tại`)
 
     const account = await user.getAccount()
 
-    sendMail(user.email, 'Lấy lại mật khẩu', `
-    <a href="http://localhost:3000/lay-lai-mat-khau/${user.id}?token=${account.password}"> Verify </a>`
+    // console.log(user.fullName);
+    // console.log(`${process.env.APP_HOST}/lay-lai-mat-khau/${user.id}?token=${account.password}`);
+    
+    // return
+
+    sendMail(user.email, 'ShioSMS - Lấy lại mật khẩu đăng nhập',
+      `
+      <div>
+        <div style="padding: 20px; background-color: #4B49AC; color: #fff; font-size: 20px;">ShioSMS</div>
+
+        <div style="padding: 20px">
+          <p style="font-size: 20px; margin: 20px 0;">
+            Xin chào <b>${user.fullName}</b>,
+            Chúng tôi nhận được yêu cầu đặt lại mật khẩu của bạn. Nếu bạn không yêu cầu, bạn có thể bỏ qua email này. Nếu thực sự bạn quên mật khẩu, hãy click vào nút bên dưới này để ShioSMS đặt lại mật khẩu cho tài khoản.
+          </p>
+          <a 
+            href="${process.env.APP_HOST}/lay-lai-mat-khau/${user.id}?token=${account.password}"
+            style="text-decoration: none; padding: 10px 15px;  background-color: #4B49AC; color: #fff; font-size: 15px; border-radius: 10px;"
+          > Lấy lại mật khẩu </a>
+        </div>
+
+      </div>
+      `
     )
 
     req.flash('successMsg', `Gửi mail xác thực thành công`)
@@ -138,7 +159,7 @@ const getResetPassword = async (req, res, next) => {
   try {
     res.render('auth/reset-password', {
       documentTitle: 'Lấy lại mật khẩu',
-      data: { id, token}
+      data: { id, token }
     })
   } catch (error) {
     switch (error.code) {
@@ -164,9 +185,9 @@ const postResetPassword = async (req, res, next) => {
   const { token } = req.query
   const { newPassword, newPassword2 } = req.body
   try {
-    if(!newPassword || !newPassword2) throw customError(2, `Vui lòng nhập đầy đủ thông tin`)
-    if(newPassword !== newPassword2) throw customError(2, `Mật khẩu không trùng khớp`)
-    if(newPassword < 6 || newPassword2 < 6) throw customError(2, `Mật khẩu phải từ 6 ký tự trở lên`)
+    if (!newPassword || !newPassword2) throw customError(2, `Vui lòng nhập đầy đủ thông tin`)
+    if (newPassword !== newPassword2) throw customError(2, `Mật khẩu không trùng khớp`)
+    if (newPassword < 6 || newPassword2 < 6) throw customError(2, `Mật khẩu phải từ 6 ký tự trở lên`)
 
     const result = await accountService.forgotPassword(id, token, newPassword)
 
@@ -178,12 +199,12 @@ const postResetPassword = async (req, res, next) => {
       case 0:
         err.type = 'errorMsg'
         err.message = error.message
-        err.url =  `/lay-lai-mat-khau/${id}?token=${token}`
+        err.url = `/lay-lai-mat-khau/${id}?token=${token}`
         break;
       case 1:
         err.type = 'errorMsg'
         err.message = error.message
-        err.url =  `/lay-lai-mat-khau/${id}?token=${token}`
+        err.url = `/lay-lai-mat-khau/${id}?token=${token}`
         break;
       case 2:
         err.type = 'formMsg'
